15a344345e3e10c2af714720dcb34e42dfcb6307
aayu-11 <ayushkumar.ak2409@gmail.com>
2024-03-28 18:58:43 +0530

    math and number

---
+++ 01_basics/07_numbers.js
@@ -0,0 +1,42 @@
+const score = 300
+// console.log(score)
+const balance = new Number(500)
+// console.log(balance)
+
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(2))
+
+const otherNumber = 23.343
+// console.log(otherNumber.toPrecision(4))
+
+const hundreds = 100000000
+// console.log(hundreds.toLocaleString('en-IN'))
+
+// console.log(Number.MAX_VALUE)
+// console.log(Number.MIN_VALUE)
+// console.log(Number.MAX_SAFE_INTEGER)
+
+
+// +++++++++++++++++++++ Maths ++++++++++++++++++
+// console.log(Math)  // itselt is a object whicch contain multiple properties
+// console.log(Math.abs(-34))
+// console.log(Math.round(4.9))
+// console.log(Math.ceil(4.9))
+// console.log(Math.floor(4.9))
+
+// sqrt()
+// pow()
+// sign()
+// sin()
+// tan()
+// max()
+// min()
+
+console.log(Math.random())  // value --> between 0 and 1
+console.log(Math.floor(Math.random() * 10) + 1)
+
+const max = 15
+const min = 10
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min) // will generate a random value between min and max
+
