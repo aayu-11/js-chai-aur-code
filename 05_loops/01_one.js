8a43fe04fea3f459eb80a07615390920ac78827d
aayu-11 <ayushkumar.ak2409@gmail.com>
2024-04-03 21:48:43 +0530

    basic loops

---
+++ 05_loops/01_one.js
@@ -0,0 +1,29 @@
+// for loop
+// for (let index = 0; index < 10; index++) {
+//   console.log(index);
+// }
+
+
+// outer and inner loop both of which run 10 times
+// for (let i = 0; i < 10; i++) {
+//   console.log(`outer loop ${i}`);
+//   for (let j = 0; j < 10; j++) {
+//     console.log(`inner loop ${j} , outer loop ${i}`);
+//   }
+// }
+
+// let myArray = ['flash', 'arrow', 'superman', 'batman', 'wonder'];
+// for (let i = 0; i < myArray.length; i++) {
+//   console.log(myArray[i]);
+// }
+
+// break and continue
+// for (let i = 0; i < 10; i++) {
+//   if (i === 5) {
+//     continue;
+//   }
+//   if (i === 8) {
+//     break;
+//   }
+//   console.log(i);
+// }
---
+++ 05_loops/02_two.js
@@ -0,0 +1,22 @@
+// while loop
+
+// let i = 0;
+// while (i < 10) {
+//   console.log(i);
+//   i++;
+// }
+
+// let arr = ['flash', 'arrow', 'superman', 'batman', 'wonder'];
+// let i = 0;
+// while (i < arr.length) {
+//   console.log(`index ${i} : ${arr[i]}`);
+//   i++;
+// }
+
+
+// do while loop
+// let i = 0;
+// do {
+//   console.log(i);
+//   i++;
+// } while (i < 10);
