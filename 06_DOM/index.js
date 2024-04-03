65edaed3549b93fc4e09019880e560524c3bde46
aayu-11 <ayushkumar.ak2409@gmail.com>
2024-04-04 01:45:19 +0530

    basic Dom manipulation

---
+++ 06_DOM/index.js
@@ -0,0 +1,48 @@
+// DOM
+// window parameters :
+// window.innerWidth
+// window.innerHeight
+// window.outerWidth
+// window.outerHeight
+// window.location
+// window.location.href
+// etc etc
+
+// window --> DOM --> Document Object Model --> HTML
+// document --> HTML
+// Dom Tree
+// document.body
+// document.head
+// document.title
+// document.URL
+// document.links
+// document.images
+// document.scripts
+// document.forms
+// etc etc
+
+// Selectors in dom
+// document.getElementById('id')
+// document.getElementsByClassName('class')
+// document.getElementsByTagName('tag')
+// document.querySelector('css selector')
+// document.querySelectorAll('css selector')
+// document.getElementsByName('name')
+// document.getElementById('id').innerHTML --> get the inner html of the element
+// document.getElementById('id').innerText --> get the inner text of the element
+// etc etc
+
+// dovument.getElementById('id').innerHTML = 'Hello World';
+// document.getElementById('id').style
+// document.getElementById('id').classList
+// document.getElementById('id').getAttribute('attribute')
+// document.getElementById('id').setAttribute('attribute', 'value')
+
+
+// html collection and node list are not pure arrays
+// html collection and node list are array like objects
+// html collection doesn't have forEach method but node list has forEach method
+// html collection and node list are live collections
+// html collection and node list are updated automatically when the dom is updated
+// html collection and node list can be converted to array using Array.from() method
+
