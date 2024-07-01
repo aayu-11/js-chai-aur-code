e9cf91df7cdae12f9aa04e276add944d568fc2ae
aayu-11 <ayushkumar.ak2409@gmail.com>
2024-07-01 15:28:08 +0530

    document formatted

--- 11_classes_and_Oops/03_prototype.js
+++ 11_classes_and_Oops/03_prototype.js
@@ -1,55 +1,56 @@
 // let myName = 'hitesh     '
 
 // console.log(myName.truelength)
-// objective --> create a method truelength which will give orignal length of string without whitespaces 
+// objective --> create a method truelength which will give orignal length of string without whitespaces
 
-let myHeros = ['thro', 'spiderman']
+let myHeros = ["thro", "spiderman"];
 
 let heroPower = {
-  thor: 'hammer',
-  spiderman: 'sling',
+  thor: "hammer",
+  spiderman: "sling",
   getSpiderPower: function () {
-    console.log(`spidy power is ${this.spiderman}`)
-  }
-}
+    console.log(`spidy power is ${this.spiderman}`);
+  },
+};
 Object.prototype.hitesh = function () {
-  console.log('hitesh is present in all objects')
-}
+  console.log("hitesh is present in all objects");
+};
 // heroPower.hitesh()
 // myHeros.hitesh()
 
 Array.prototype.heyHitesh = function () {
-  console.log('hitesh says hello')
-}
-myHeros.heyHitesh()
+  console.log("hitesh says hello");
+};
+myHeros.heyHitesh();
 // heroPower.heyHitesh()  // object doesn't have access to user defined Array prototype function
 
 const user = {
-  name: 'chai',
-  email: 'chai@google.com'
-}
+  name: "chai",
+  email: "chai@google.com",
+};
 const Teacher = {
-  makeVideo: true
-}
+  makeVideo: true,
+};
 const TeachingSupport = {
-  isAvilable: false
-}
+  isAvilable: false,
+};
 const TAsupport = {
-  makeAssignment: 'JS Assignment',
+  makeAssignment: "JS Assignment",
   fullTime: true,
-  __proto__: TeachingSupport
-}
-Teacher.__proto__ = user // example of prototypel inheritance
+  __proto__: TeachingSupport,
+};
+Teacher.__proto__ = user; // example of prototypel inheritance
 
 // modern syntax
-Object.setPrototypeOf(TeachingSupport, Teacher)
+Object.setPrototypeOf(TeachingSupport, Teacher);
 
-let anotherUserName = 'chaiAurCode     '
+let anotherUserName = "chaiAurCode     ";
 String.prototype.trueLength = function () {
-  console.log(`${this}`)
-  console.log(`True length is : ${this.trim().length}`)
-}
+  console.log(`${this}`);
+  console.log(`True length is : ${this.trim().length}`);
+};
 
-anotherUserName.trueLength()
-'ayush'.trueLength()
-'hello'.trueLength()
+anotherUserName.trueLength();
+"ayush".trueLength();
+"hello".trueLength();
+
