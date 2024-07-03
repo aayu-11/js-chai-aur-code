d252b5776bfe67de8ad9e8fa1d8725e491bc6046
aayu-11 <ayushkumar.ak2409@gmail.com>
2024-07-03 23:22:12 +0530

    commit

--- 11_classes_and_Oops/04_call.js
+++ 11_classes_and_Oops/04_call.js
@@ -1,13 +1,13 @@
 function setUserName(username) {
   // complex db calls
-  this.setUserName = username
-  console.log('called')
+  this.setUserName = username;
+  console.log("called");
 }
 function createUser(username, email, password) {
-  setUserName.call(this, username)
-  this.email = email
-  this.password = password
+  setUserName.call(this, username);
+  this.email = email;
+  this.password = password;
 }
 
-const chai = new createUser('chai', 'exxample@gmail.com', '123')
-console.log(chai)
+const chai = new createUser("chai", "exxample@gmail.com", "123");
+console.log(chai);
