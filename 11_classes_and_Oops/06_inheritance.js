eda6ea77de314248104ae5614299c4aed57c0ea2
aayu-11 <ayushkumar.ak2409@gmail.com>
2024-07-06 20:14:27 +0530

    commit

--- 11_classes_and_Oops/06_inheritance.js
+++ 11_classes_and_Oops/06_inheritance.js
@@ -1,24 +1,26 @@
 class User {
   constructor(username) {
-    this.username = username
+    this.username = username;
   }
   logMe() {
-    console.log(`USERNAME IS ${this.username}`)
+    console.log(`USERNAME IS ${this.username}`);
   }
 }
+
 class Teacher extends User {
   constructor(username, email, password) {
-    super(username)
-    this.email = email
-    this.password = password
+    super(username);
+    this.email = email;
+    this.password = password;
   }
   addCourse() {
-    console.log(`A new course was added by ${this.username}`)
+    console.log(`A new course was added by ${this.username}`);
   }
 }
-const chai = new Teacher('hitesh', 'chai@124.com', 1243)
-chai.addCourse()
+const chai = new Teacher("hitesh", "chai@124.com", 1243);
+chai.addCourse();
 
-const masalaChai = new User('masalaChai')
-masalaChai.logMe()
-console.log(chai instanceof Teacher)
+const masalaChai = new User("masalaChai");
+masalaChai.logMe();
+console.log(chai instanceof Teacher);
+
